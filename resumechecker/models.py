ffc509b79889ab6ef1e9b271bcd3f7de9ea554da
phitron-demo <sojibhasan5800@gmail.com>
2025-09-17 16:11:34 +0600

    Added Resume and JobDescription models with admin registration for resume checker app

--- resumechecker/models.py
+++ resumechecker/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 
-# Create your models here.
+
+class Resume(models.Model):
+    resume = models.FileField(upload_to="resume")
+
+
+class JobDescription(models.Model):
+    job_title = models.CharField(max_length=100)
+    job_description = models.TextField()
+
+    def __str__(self):
+        return self.job_title
