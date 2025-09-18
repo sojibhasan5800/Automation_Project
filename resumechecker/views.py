abf1db17038eb313cf76215371710b5caa567184
phitron-demo <sojibhasan5800@gmail.com>
2025-09-18 07:49:38 +0600

    Built AnalyzeResume API to upload resumes, process with LLM, and return structured analysis results

--- resumechecker/views.py
+++ resumechecker/views.py
@@ -15,3 +15,42 @@ class JobDescriptionAPI(APIView):
             'status' : True,
             'data' : serializer.data
         })
+    
+
+
+
+class AnalyzeResumeAPI(APIView):
+    def post(self , request):
+        try:
+            data = request.data
+            if not data.get('job_description'):
+                return Response({
+                    'status' : False,
+                    'message' : 'job_description is required',
+                    'data' : {}
+                })
+            
+            serializer = ResumeSerializer(data = data)
+            if not serializer.is_valid():
+                return Response({
+                        'status' : False,
+                        'message' : 'errors',
+                        'data' : serializer.errors
+                    })
+            
+            serializer.save()
+            _data = serializer.data
+            resume_instance = Resume.objects.get(id = _data['id'])
+            resume_path = resume_instance.resume.path
+            data = process_resume(resume_path, 
+                                  JobDescription.objects.get(id =data.get('job_description') ).job_description
+                                  )           
+            return Response({
+                        'status' : True,
+                        'message' : 'resume analyzed',
+                        'data' : data
+                    })
+        except Exception as e:
+            return Response({
+                'data' : False
+            })
