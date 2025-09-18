88606a1a9b77902d1f75fde5cd4a34d4bf34d77d
phitron-demo <sojibhasan5800@gmail.com>
2025-09-18 07:47:31 +0600

    Developed JobDescription API endpoint with DRF to fetch and serialize job descriptions.

--- resumechecker/views.py
+++ resumechecker/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializer import JobDescriptionSerializer, ResumeSerializer
+from .models import JobDescription,Resume
+from .analyzer import process_resume
+
+class JobDescriptionAPI(APIView):
+    def get(self , request):
+        queryset = JobDescription.objects.all()
+        serializer = JobDescriptionSerializer(queryset, many = True)
+        return Response({
+            'status' : True,
+            'data' : serializer.data
+        })
