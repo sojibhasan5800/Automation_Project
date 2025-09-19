0f3eb48bdf011f51313f75d0e9e46f5eed9a8b52
phitron-demo <sojibhasan5800@gmail.com>
2025-09-19 15:05:00 +0600

    Added Game model for TicTac app with fields for room code, players, and game state, followed by migration

--- automation_dj/urls.py
+++ automation_dj/urls.py
@@ -32,5 +32,6 @@ urlpatterns = [
     path('webscraping/', include('stockanalysis.urls')),
     path('resumecheckers/', include('resumechecker.urls')),
     path('scaners/', include('scaners.urls')),
+    path('tictacs/', include('tictac.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
