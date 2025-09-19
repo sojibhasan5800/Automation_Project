f7b500a6f97a539c2a7e2e7c73851c284fd88dde
phitron-demo <sojibhasan5800@gmail.com>
2025-09-19 11:52:35 +0600

    Enhanced QR code generator to process form data, save QR images, and render results dynamically.

--- scaners/views.py
+++ scaners/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
 from .forms import QRCodeForm
-
+import qrcode
+import os
+from django.conf import settings
 
 
 def generate_qr_code(request):
+    if request.method == 'POST':
+        form = QRCodeForm(request.POST)
+        if form.is_valid():
+            res_name = form.cleaned_data['restaurant_name']
+            url = form.cleaned_data['url']
 
+            # Generate QR Code
+            qr = qrcode.make(url)
+            file_name = res_name.replace(" ", "_").lower() + '_menu.png'
+            file_path = os.path.join(settings.MEDIA_ROOT, file_name) #../media/rathan_rest_menu.png
+            qr.save(file_path)
 
+            # Create Image URL
+            qr_url = os.path.join(settings.MEDIA_URL, file_name)
+
+            context = {
+                'res_name': res_name,
+                'qr_url': qr_url,
+                'file_name': file_name,
+            }
+            return render(request, 'qr_result.html', context)
+    
+    else:
         form = QRCodeForm()
         context = {
             'form': form,
