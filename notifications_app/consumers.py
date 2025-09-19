ec8fb2f9537b361ed035e69b7105919e2603e11e
phitron-demo <sojibhasan5800@gmail.com>
2025-09-19 16:14:56 +0600

    Implemented TicTac WebSocket consumer to handle real-time game events and broadcast moves to players.

--- notifications_app/consumers.py
+++ notifications_app/consumers.py
@@ -1,5 +1,7 @@
 import json,html
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer,WebsocketConsumer
+from asgiref.sync import async_to_sync
+import json
 
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -60,3 +62,43 @@ class NotificationConsumer(AsyncWebsocketConsumer):
             "type": type_,
         }))
 
+
+class GameRoom(WebsocketConsumer):
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_code']
+        self.room_group_name = 'room_%s' %  self.room_name
+        print(self.room_group_name) 
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        
+        self.accept()
+
+        
+    def disconnect(self):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+        
+    def receive(self , text_data):
+        print(text_data)
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,{
+                'type' : 'run_game',
+                'payload' : text_data
+            }
+        )
+        
+    
+    def run_game(self , event):
+        data = event['payload']
+        data = json.loads(data)
+
+        self.send(text_data= json.dumps({
+            'payload' : data['data']
+        }))        
+        
+         
