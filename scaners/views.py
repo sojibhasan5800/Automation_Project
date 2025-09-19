45e25d756c10446aa06a41e5a33f4b7b593a69b3
phitron-demo <sojibhasan5800@gmail.com>
2025-09-19 11:32:32 +0600

    Implemented QR code generator view, form, template, and URL routing in Scaners app

--- scaners/views.py
+++ scaners/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import QRCodeForm
 
-# Create your views here.
+
+
+def generate_qr_code(request):
+
+
+        form = QRCodeForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'generate_qr_code.html', context)
